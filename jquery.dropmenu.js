6ca2822f86dc9073b8755a26df91fb30d07c975f
Martijn <Martijn@Ash>
2013-06-18 22:00:55 +0200

    Callback galore

--- jquery.dropmenu.js
+++ jquery.dropmenu.js
@@ -19,6 +19,9 @@
 		options: {
             'items':			[],
 			'closeOnSelect':	true,
+			'showAnim':			'show',
+			'duration':			'normal',
+			'showOptions':		{},
 
 			'select':			undefined
         },
@@ -45,48 +48,82 @@
             return that;
         },
 
+		_show: function(element) {
+			if ($.effects && $.effects.effect[this.options.showAnim]) {
+				element.show(this.options.showAnim, this.options.showOptions, this.options.duration);
+			} else {
+				element[this.options.showAnim || "show"](this.options.showAnim ? this.options.duration : null);
+			}
+		},
+
+		_hide: function(element, callback) {
+			if ($.effects && ($.effects.effect[this.options.showAnim] || $.effects[this.options.showAnim])) {
+				element.hide(this.options.showAnim, this.options.showOptions, this.options.duration, callback);
+			} else {
+				element[(this.options.showAnim === "slideDown" ? "slideUp" :
+					(this.options.showAnim === "fadeIn" ? "fadeOut" : "hide"))]((this.options.showAnim ? this.options.duration : null), callback);
+			}
+		},
+
 		open: function() {
 			if (!this.menu) {
 				this.menu = $('<div class="dropmenu"/>').hide().appendTo('body');
 				this._addItems(this.menu, this.options.items);
 
-				this.menu.position({
+				this._show(this.menu.position({
 					'of':	this.element,
 					'my':	'left top',
 					'at':	'left bottom'
-				}).show();
+				}));
 			}
 		},
 
 		close: function() {
-			if (this.menu) {
-				this.menu.remove();
-				this.menu = null;
+			var that = this;
+			if (that.menu) {
+				that._hide(that.menu, function() {
+					that.menu.remove();
+					that.menu = null;
+				});
 			}
 		},
 
 		_addItem: function(menu, item) {
-			var that	= this,
-				label	= item.label || item.name,
-				row	= $('<div class="dropmenu-item">'+label+'</div>').appendTo(menu),
-				submenu;
+			var that		= this,
+				label		= item.label || item.name,
+				row			= $('<div class="dropmenu-item">'+label+'</div>').appendTo(menu),
+				selectable	= item.selectable ? item.selectable	: (item.items ? false : true),
+				submenu,
+				items;
 		
-			if ($.isArray(item.items)) {
+			if (item.items) {
 				row.addClass('dropmenu-parent');
 				submenu = $('<div class="dropmenu"/>').appendTo(row);
-				that._addItems(submenu, item.items);
 
 				row.hover(function() {
 					submenu.css({
 						'left': row.outerWidth(),
 						'top': row.position().top
-					}).show();
+					}).empty();
+
+					if ($.isFunction(item.items)) {
+						if (items = item.items.call(this, item, function(items) {
+							that._addItems(submenu, items);
+							that._show(submenu);
+						})) {
+							that._addItems(submenu, items);
+							that._show(submenu);
+						}
+					} else {
+						that._addItems(submenu, item.items);
+						that._show(submenu);
+					}
 				}, function() {
-					submenu.hide();
+					that._hide(submenu);
 				});
 			}
 
-			if (item.selectable !== false) {
+			if (selectable !== false) {
 				row.addClass('dropmenu-selectable');
 				row.click(function() {
 					if ($.isFunction(item.select)) {
@@ -105,7 +142,15 @@
 			var that = this;
 
 			$.each(items, function(index, item) {
-				that._addItem(menu, item);
+				if ($.isFunction(item)) {
+					if (item = item.call(this, item, function(item) {
+						that._addItem(menu, item);
+					})) {
+						that._addItem(menu, item);
+					}
+				} else {
+					that._addItem(menu, item);
+				}
 			});
 		}
     });
